29a156f8588869d44c354a6a0c81e7b0c1b6fcec
Colin M <colinm999123@gmail.com>
2021-04-29 22:03:51 +0100

    Code cleanup

--- discordbot/helpers/__init__.py
+++ discordbot/helpers/__init__.py
@@ -2,3 +2,4 @@ from .roles import DiscordRoles
 from .dcsserver import DcsServer
 from .dcsserverrepository import DcsServerRepository
 from .dcsmissioneditor import DcsMissionEditor
+from .units import Units
