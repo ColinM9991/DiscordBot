d2bc3bbf3c369581776f6af9bbe29d0ab8577745
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2022-12-14 22:12:53 +0000

    Add weather mapping

--- discordbot/helpers/__init__.py
+++ discordbot/helpers/__init__.py
@@ -1,3 +1,5 @@
 from .weatherservice import WeatherService
 from .openmapweatherservice import OpenMapWeatherService, CityNotFoundError
 from .roles import DiscordRoles
+from .dcsserver import DcsServer, ConcreteDcsServer
+from .dcsweathermapper import DcsWeatherMapper
