be6230c095422d625115b2022d018dbd2cfe525d
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2022-12-14 22:12:52 +0000

    Restructure folders and add requirements.txt

--- discordbot/DcsServer.py
+++ discordbot/DcsServer.py
@@ -3,4 +3,4 @@ import psutil as psutil
 
 class DcsServer:
     def is_running(self):
-        "dcs" in (p.name() for p in psutil.process_iter())
+        return "dcs" in (p.name() for p in psutil.process_iter())
