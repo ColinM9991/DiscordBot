54188bd639b44ba1ac95568976778a4a00f27269
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2022-12-14 22:12:53 +0000

    Improve the multi instance support. I'd love to unit test this but I have no idea where I'm taking this yet.

--- discordbot/cogs/dcsservercog.py
+++ discordbot/cogs/dcsservercog.py
@@ -1,19 +1,25 @@
 from discord.ext import commands
-from helpers.dcsserver import ConcreteDcsServer, DcsServer
+from helpers.dcsserver import MultiInstanceDcsServer
 from os import environ
 
 
 class DcsServerCog(commands.Cog, name="DCS Server Commands"):
-    def __init__(self, bot, dcs_server: DcsServer):
+    def __init__(self, bot, dcs_server: MultiInstanceDcsServer):
         self.bot = bot
-        self.dcs_server: DcsServer = dcs_server
+        self.dcs_server: MultiInstanceDcsServer = dcs_server
 
     @commands.command(help="Checks the status of the DCS server.")
-    async def check_server(self, ctx):
-        if self.dcs_server.is_running():
-            await ctx.send('The server is currently running')
+    async def check_server(self, ctx, instance):
+        try:
+            dcs_instance = self.dcs_server.get_instance(instance)
+        except ValueError:
+            await ctx.send('Invalid instance specified')
+            raise
+
+        if dcs_instance.is_running():
+            await ctx.send(f'The {dcs_instance.get_instance_name} server is currently running')
         else:
-            await ctx.send('The server is currently offline')
+            await ctx.send(f'The {dcs_instance.get_instance_name} server is currently offline')
 
     @commands.command(help='Returns a list of registered instances.')
     async def get_instances(self, ctx):
@@ -23,4 +29,6 @@ class DcsServerCog(commands.Cog, name="DCS Server Commands"):
 
 
 def setup(bot):
-    bot.add_cog(DcsServerCog(bot, ConcreteDcsServer(environ.get('DCS_PROFILE_PATH'))))
+    bot.add_cog(DcsServerCog(bot, MultiInstanceDcsServer(
+                                  environ.get('DCS_PROFILE_PATH'),
+                                  environ.get('FIREDAEMON_CONFIG_PATH'))))
